1c250d2f706d7a0b9f2f47eb7d531397f37792d9
Federico Pacheco <federicogpacheco@gmail.com>
2015-02-12 22:46:39 -0300

    +style img +anmer oculto

--- javas.js
+++ javas.js
@@ -2,3 +2,10 @@
  * @author Federico Pacheco
  */
 
+var script = document.createElement('script');
+script.src = "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js";
+document.head.appendChild(script);
+
+function toAnmer(){
+	document.location = CryptoJS.MD5(prompt())+"/index.html";
+}
